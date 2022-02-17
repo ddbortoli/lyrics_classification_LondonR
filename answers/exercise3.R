4c7d407d0012994462b203d7b16ba6b35db1aa09
Daniel de Bortoli <daniel.debortoli@mango-solutions.com>
2022-02-17 12:45:14 +0000

    Expand exercise 3 solution

--- answers/exercise3.R
+++ answers/exercise3.R
@@ -4,3 +4,13 @@
 # words in the lyrics. Therefore looking at the 'most important' feature is not 
 # going to tell us anything about specific words in the lyrics, unlike for 
 # TFIDF features where each value was related to a specific word.
+
+embed_rf_fit <- fit(embed_wf, lyrics_train)
+
+# We can then find which of the features were most important when classifying
+# genre. This can be done using the {vip} package.
+
+embed_rf_fit %>% 
+  extract_fit_parsnip() %>% 
+  vip(scale = TRUE) + 
+  labs(title = "Variable Importance Scores for random forest model")
